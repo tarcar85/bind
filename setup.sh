97ea929130af61c3648f17f82070baadd577ae70
secobau <sebastian.colomar@gmail.com>
2019-09-25 19:08:40 +0200

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -5,10 +5,14 @@ external=eth0
 internal=eth1
 service=dns
 ####################
+sudo yum update -y
+sudo yum install -y firewalld
+sudo systemctl enable firewalld
 sudo systemctl start firewalld
 sudo firewall-cmd --change-interface=$external --zone=external --permanent
 sudo firewall-cmd --change-interface=$internal --zone=internal --permanent
 sudo firewall-cmd --reload
+####################
 sudo yum install -y bind bind-utils
 sudo systemctl enable named
 sudo firewall-cmd --add-service=$service --zone=internal --permanent
