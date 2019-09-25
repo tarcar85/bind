6c35be75747dafe45e24b0571f1e420e446a4f90
secobau <sebastian.colomar@gmail.com>
2019-09-25 18:50:49 +0200

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,17 +1,20 @@
 ####################
-dns=127.0.0.1
+localhost=127.0.0.1
 domain=LOCAL.DO
+external=eth0
+internal=eth1
+service=dns
 ####################
 sudo systemctl start firewalld
-sudo firewall-cmd --change-interface=eth1 --zone=internal --permanent
-sudo firewall-cmd --change-interface=eth0 --zone=external --permanent
+sudo firewall-cmd --change-interface=$external --zone=external --permanent
+sudo firewall-cmd --change-interface=$internal --zone=internal --permanent
 sudo firewall-cmd --reload
 sudo yum install -y bind bind-utils
 sudo systemctl enable named
-sudo firewall-cmd --add-service=dns --zone=internal --permanent
+sudo firewall-cmd --add-service=$service --zone=internal --permanent
 ####################
 sudo cp -rv * /
-echo -e "DNS1=$dns\nDOMAIN=$domain" | sudo tee -a /etc/sysconfig/network-scripts/ifcfg-$internal
+echo -e "DNS1=$localhost\nDOMAIN=$domain" | sudo tee -a /etc/sysconfig/network-scripts/ifcfg-$internal
 ####################
 sudo init 6
 ####################
